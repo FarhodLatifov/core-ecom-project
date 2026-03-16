f674eb8eeb2f1b04169b50a67742399b0ea46f9a
FarhodLatifov <latifovfarhod@gmail.com>
2026-03-16 21:10:39 +0500

    deleted unused Test.tsx

--- src/Test.tsx
+++
@@ -1,9 +0,0 @@
-import { useSelector, useDispatch } from 'react-redux';
-import type { RootState, AppDispatch } from './store/store';
-
-export function Test() {
-  const dummy = useSelector((state: RootState) => state.dummy);
-  const dispatch: AppDispatch = useDispatch();
-
-  return <div>{JSON.stringify(dummy)}</div>;
-}
